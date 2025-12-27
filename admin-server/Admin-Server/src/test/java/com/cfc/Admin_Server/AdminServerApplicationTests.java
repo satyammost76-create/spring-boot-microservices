7e297d914ad3d3593a1e1e842101310639d5f719
satyam kumar <satyammost76@gamil.com>
2025-12-27 15:52:15 +0530

    add admin-server

---
+++ admin-server/Admin-Server/src/test/java/com/cfc/Admin_Server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfc.Admin_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
